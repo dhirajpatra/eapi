ffd178c331aadefa3f048f78a3163cab9d36741e
Dhiraj Patra <dhiraj.patra@gmail.com>
2020-02-27 13:49:23 +0530

    updated

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,14 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
+});
+
+Route::apiResource('/products', 'ProductController');
+
+Route::group(['prefix' => 'products'], function () {
+	Route::apiResource('/{product}/reviews', 'ReviewController');
 });
